2482c4301fcd3ec86feb459826a469e53c1aed24
Tassa Tenhouse <kmtenhouse@gmail.com>
2018-09-30 15:40:15 -0700

    Fixing merge issue

---
+++ assets/javascript/secret.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC0xlClqFPpUsNP4uY4wdOamzJKR_jzWBg",
+    authDomain: "projectfun-a97ed.firebaseapp.com",
+    databaseURL: "https://projectfun-a97ed.firebaseio.com",
+    projectId: "projectfun-a97ed",
+    storageBucket: "projectfun-a97ed.appspot.com",
+    messagingSenderId: "96611333950"
+  };
+  firebase.initializeApp(config);
